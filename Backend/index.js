48839d7c6d5be21280ea32b99c7a316af313e6fa
kimwong9999 <103122457+kimwong9999@users.noreply.github.com>
2022-06-29 13:38:34 +0100

    added links for searching movies

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-var fs = require('fs');
-const { response } = require('express');
+const express = require('express')
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+var fs = require('fs')
+const { response } = require('express')
 const cors = require('cors'); // new
 const bodyParser = require('body-parser') // new
 
 
-const app = express() // new
+const app = express()
 app.use(cors({origin:true})) // new
 
 app.use(bodyParser.json()); // new
@@ -15,8 +15,7 @@ app.use(bodyParser.urlencoded({ // new
     extended: true
 }));
 
-const uri = 'mongodb://localhost:27017/qacinemas';
-let opts = { useNewUrlParser: true }
+
 
 
 const movieSchema = new Schema 
@@ -44,6 +43,9 @@ const locationSchema = new Schema
 
 const Movie = mongoose.model('Movie', movieSchema)
 const Location = mongoose.model('Location', locationSchema)
+
+const uri = 'mongodb://localhost:27017/qacinemas';
+let opts = { useNewUrlParser: true }
 
 mongoose.connect
 (uri, opts,
@@ -121,6 +123,112 @@ app.get
     }
 )
 
+
+
+app.get
+('/movie/select/:id', function(request, response)
+    {
+        let id = request.params.id
+
+        Movie.find
+        ({'id':id}, (error, movie) =>
+            {
+                if (error)
+                {
+                    console.log('Error /movie/select/' + id + ': ' + error)
+                    movie = [{"id":0, "title":'Error /movie/select/ ' + id + ': ' + error}]
+                }
+
+                if (movie == '')
+                {
+                    movie = [{"id":0, "title":'Cannot find a movie title with the id: ' + id}]
+                }
+
+                response.send(movie)
+            }
+        )
+    }
+)
+
+app.get
+('/movie/search/genre/:genre', function(request, response)
+    {
+        let genre = request.params.genre
+
+        Movie.find
+        ({'genre':genre}, (error, movies) =>
+            {
+                let list = []
+
+                if (error)
+                {
+                    console.log('Error /movie/select/genre/'+ genre + ': ' + error)
+                    list.push({"id":0, "title":'Error /movie/select/genre/' + genre + ': ' + error})
+                }
+                else
+                {
+                    if (movies == '')
+                    {
+                        list.push("No movie found for genre: " + genre)
+                    }
+                    else
+                    {
+                        movies.forEach
+                        ((movie) =>
+                            {
+                                list.push(movie)
+                            }
+                        )
+                    }
+                }
+                
+                response.send(list)
+            }
+
+        )
+    }
+)
+
+app.get
+('/movie/search/title/:title', function(request, response)
+    {
+        let title = request.params.title
+
+        Movie.find
+        ({'title':{$regex: title, $options: 'i'}}, (error, movies) =>
+            {
+                let list = []
+
+                if (error)
+                {
+                    console.log('Error /movie/select/title/'+ title + ': ' + error)
+                    list.push({"id":0, "title":'Error movie/select/title/' + title + ': ' + error})
+                }
+                else
+                {
+                    console.log('movies' + movies)
+                    if (movies == '')
+                    {
+                        list.push("No movie title found using the search criteria: " + title)
+                    }
+                    else
+                    {
+                        movies.forEach
+                        ((movie) =>
+                            {
+                                list.push(movie)
+                            }
+                        )
+                    }
+                }
+                
+                response.send(list)
+            }
+
+        )
+    }
+)
+
 app.get
 ('/locationlist', function(request, response)
     {
@@ -139,7 +247,7 @@ app.get
                     locations.forEach
                     ((location) =>
                         {
-                           list.push({"id":location.id, "title":location.city})
+                           list.push({"id":location.id, "city":location.city})
                         }
                     )
                 }
