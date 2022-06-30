411312ee48f25dfabc5c54a221165cf9d33a45da
Peter-Dagley <peter_dagley@outlook.com>
2022-06-30 09:32:30 +0100

    added new releases API to page

--- Frontend/moviewebsite/src/components/Pages/NewReleasesGallery/NewReleasesGallery.js
+++ Frontend/moviewebsite/src/components/Pages/NewReleasesGallery/NewReleasesGallery.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import NewReleasesAPI from './NewReleasesAPI'
 
 const NewReleasesGallery = () => {
   return (
     <>
-    <div>NewReleasesGallery</div>
+    <div className='container-fluid'>
+      <div className='container-lg'>
+          
+          <div className='container' id='header'>
+            <h1 className='text1'>New Releases</h1>
+          </div>
+
+          <br></br>
+
+          <div className='container'>
+            <h2 className='text1'></h2>
+            <NewReleasesAPI />
+            
+          </div>
+
+      </div>
+    </div>
     </>
   )
 }
