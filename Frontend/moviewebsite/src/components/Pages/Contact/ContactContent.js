13397ddbd269fc13ac191d6927fbd239422c9d11
Peter-Dagley <peter_dagley@outlook.com>
2022-06-30 16:14:11 +0100

    changed file structure to add content files for components

---
+++ Frontend/moviewebsite/src/components/Pages/Contact/ContactContent.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const ContactContent = () => {
+  return (
+    <div>
+    <h1> Contact Page</h1>
+    <p>Please feel free to contact us for any enquiries</p>
+
+    <h2>Contact us via email</h2>
+    <form action="https://formsubmit.co/87393ec81fcbba0211622f33c93f1126" method="POST">
+    <label for="fname">First Name</label>
+      <input type="text" placeholder="Your name.." ></input>
+  
+      <label for="lname">Last Name</label>
+      <input type="text" id="lname" name="lastname" placeholder="Your last name.."></input>
+
+      <label for="email">Email</label>
+      <input type="email" name="email" placeholder="Your email address..." ></input>
+
+      <label for="Subject">Enter Subject </label>
+      <input type="textarea" name="textValue" placeholder="Enter content here"></input>
+  
+      
+      <button type="submit">Send</button>
+     </form> <br/>          
+    <h2>Contact us via post</h2>
+    <p>435 Cinema Street</p>
+    <p>London</p>
+    <p>CE15 4AU</p>
+
+    <h2>Contact us via phone</h2>
+    <p>+44 208 675 6554</p>
+
+
+  </div>
+  )
+}
+
+export default ContactContent
