7f328165f63b57d43d882d214c9602c4f6e53110
SalmaH98 <103117561+SalmaH98@users.noreply.github.com>
2022-07-05 10:13:17 +0100

    Upload homepage

--- Frontend/moviewebsite/src/components/Pages/Home/Home.js
+++ Frontend/moviewebsite/src/components/Pages/Home/Home.js
@@ -4,13 +4,29 @@ import './HomeStyles.css'
 
 const Home = () => {
   return (
-  <div className='container-fluid'>
     <div className='container-lg'>
         
         <div className='container' id='header'>
           <h1 className='text1'>QA Cinemas</h1>
         </div>
+<div className="column">
+                    <h3> Welcom to QA Cinema </h3>
+                    Welcome to QA Cinema Home of the latest and greatest Blockbuster films right now.
+                    <br/>
+              
+                    <div className="column" >
+                    <img src={HomeImg} alt="Cinema-Picture.jpeg" width='555' />   <img src={HometImage} alt="Seating-Picture" width='600'  /><br/>
+                    </div>
+                    <br/>
+                    
+                    We always try to strive to provide high quality entertainment at budget friendly prices.
+                    <Link to="/ticketbookings"><u>here</u></Link>. <br/>
 
+                    We are always trying to improve our services to ensure the best experiances for all our customers.
+                    Please do not hesistate to contact us or leave a message in the discussion board<Link to="/discussionboard"><u>here</u></Link>
+ <div>
+ </div>
+ </div>
         <br></br>
 
         <div className='container'>
@@ -23,5 +39,4 @@ const Home = () => {
   </div>
   )
 };
-
 export default Home;
