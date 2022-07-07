8720a6afc696751eeec03fe810269220012e99c4
kimwong9999 <103122457+kimwong9999@users.noreply.github.com>
2022-07-07 13:36:25 +0100

    minor changes to screen

--- Frontend/moviewebsite/src/components/Pages/Screens/Screens.js
+++ Frontend/moviewebsite/src/components/Pages/Screens/Screens.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import { Link } from "react-router-dom"
 import ScreenImg1 from "../../Images/Img/Screen-1.jpg"
 import ScreenImg2 from "../../Images/Img/Screen-2.jpg"
 import ScreenImg3 from "../../Images/Img/Screen-3.jpg"
 import ScreenImg4 from "../../Images/Img/Screen-4.jpg"
+import "./ScreensStyles.css"
 
 
 const Screens = () => {
   return (
-    <div>
+    <div className="page" align="center">
       
-   <h3 className='primary'> Screen 1 - Standard</h3>
+   <h3> Screen 1 - Standard</h3>
    <img src={ScreenImg1} alt='Screen-1' width="600" height="500" /><br/>
    <br/>
    <br/>
@@ -20,7 +20,7 @@ const Screens = () => {
    <br/>
 
  
-   <h3 className='primary'> Screen 2 - Standard </h3>
+   <h3> Screen 2 - Standard </h3>
    <img src={ScreenImg2} alt='Screen-2' width="600" height="500" /><br/>
    <br/><br/>
    <br/>
@@ -30,7 +30,7 @@ const Screens = () => {
    <br/>
 
 
-   <h3 className='primary'> Screen 3 - Standard </h3>
+   <h3> Screen 3 - Standard </h3>
    <img src={ScreenImg3} alt="Screen-3" width="600" height="500" /><br/>
    <br/><br/>
    <br/>
@@ -40,7 +40,7 @@ const Screens = () => {
    <br/>
 
 
-   <h3 className='primary'> Screen 4 - IMAX </h3>
+   <h3> Screen 4 - IMAX </h3>
    <img src={ScreenImg4} alt="Screen-4" width="600" height="500" /><br/>
    <br/><br/>
    <br/>
@@ -48,9 +48,6 @@ const Screens = () => {
    <br/>
    <br/>
    <br/>
-
-
-
 
 
     </div>  
